e600ea5c0aa05d2c451c6a78a30d0d61c2daaf8d
fukuchiharuki <fukuchi.haruki@gmail.com>
2023-05-06 11:04:49 +0900

    feat: keyboard

---
+++ components/KeyboardSideKey.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="keyboard-key keyboard-key--side">
+    <button>{{ label }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    label: string
+  }>()
+</script>
+
+<style lang="scss" scoped>
+  .keyboard-key--side {
+    button {
+      background-color: gray;
+      color: white;
+    }
+  }
+</style>
---
+++ components/VirtualDisplay.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="display">Display</div>
+</template>
+
+<style lang="scss" scoped>
+  //.display {
+  //}
+</style>
