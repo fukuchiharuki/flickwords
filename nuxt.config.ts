5c3593bd5f889408bdbe382e49b6a9ee908f85fd
fukuchiharuki <fukuchi.haruki@gmail.com>
2023-05-06 21:25:38 +0900

    chore: base.href

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/flic-input-ja/'
+    baseURL: '/flic-input-ja/',
+    head: {
+      title: 'My Blog',
+      base: { href: '/flic-input-ja/' }
+    }
   },
   css: ['@/assets/styles/main.scss'],
   vite: {
