c15aedb1b338bc81a4aa5f1b72eb5de7eac09cde
fukuchiharuki <fukuchi.haruki@gmail.com>
2023-05-06 19:47:21 +0900

    debug: alert

--- components/VirtualDisplay.vue
+++ components/VirtualDisplay.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="display">Display</div>
+  <div class="display" @click="test('click display')">Display</div>
 </template>
+
+<script setup lang="ts">
+  function test(v: string) {
+    alert(v)
+  }
+</script>
 
 <style lang="scss" scoped>
   //.display {
