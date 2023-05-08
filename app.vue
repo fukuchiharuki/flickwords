216fc392ee61a38286e177c3817af069b1b94fec
fukuchiharuki <fukuchi.haruki@gmail.com>
2023-05-08 16:31:31 +0900

    refactor: VirtualConsole

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <VirtualConsole>
+      <template #display>
+        <VirtualDisplay />
+      </template>
+      <template #keyboard>
+        <VirtualKeyboard @input="onInput" />
+      </template>
+    </VirtualConsole>
+  </div>
+</template>
+
+<script setup lang="ts">
+  function onInput(args: { type: string; value: string }) {
+    const { type, value } = args
+    console.log(type, value)
+  }
+</script>
