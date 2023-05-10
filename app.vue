45ec2fa55f5e6440e201ad97d8ddfbf8dd37c68c
fukuchiharuki <fukuchi.haruki@gmail.com>
2023-05-10 10:41:45 +0900

    feat: 改行なし

--- app.vue
+++ app.vue
@@ -12,12 +12,11 @@
 </template>
 
 <script setup lang="ts">
-  const { text, input, backspace, newline } = useTextEdit(5)
+  const { text, input, backspace } = useTextEdit(5)
 
   function onInput(args: { type: string; value: string }) {
     const { type, value } = args
     if (type === 'kana') input(value)
     if (type === 'func' && value === 'backspace') backspace()
-    if (type === 'func' && value === 'enter') newline()
   }
 </script>
