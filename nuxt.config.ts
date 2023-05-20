f5b5e28a5537ddae06325ab92afef0d33c6a01fd
fukuchiharuki <fukuchi.haruki@gmail.com>
2023-05-20 17:57:20 +0900

    feat: faviconとタイトル等

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,33 @@
 export default defineNuxtConfig({
   ssr: false,
   app: {
-    baseURL: '/'
+    baseURL: '/',
+    head: {
+      title: 'Flickwords',
+      meta: [
+        { name: 'description', content: 'A flick input Japanese word game.' },
+        {
+          name: 'keywords',
+          content: 'flickwords,flick,input,japanese,word,game,wordle'
+        }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/vnd.microsoft.icon', href: '/favicon.ico' },
+        {
+          rel: 'stylesheet',
+          href: 'https://unpkg.com/balloon-css/balloon.min.css'
+        }
+      ],
+      script: [
+        {
+          async: true,
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-8R8VVHM76P'
+        },
+        {
+          innerHTML: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-8R8VVHM76P');`
+        }
+      ]
+    }
   },
   css: ['@/assets/styles/main.scss'],
   vite: {
