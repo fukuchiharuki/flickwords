9f4af6e533e7cb6efc21f5ad52dbba765d5418d2
fukuchiharuki <fukuchi.haruki@gmail.com>
2023-05-21 09:11:14 +0900

    fix: リファクタリング修正漏れ

--- components/GameBoard.vue
+++ components/GameBoard.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script setup lang="ts">
-  import { Answer } from '../composables/useGameBoard'
+  import { Answer } from '~/repositories/Answer'
 
   defineProps<{
     answer: Answer
