7d9d86f86db4ce8facd050435e6c999767350087
fukuchiharuki <fukuchi.haruki@gmail.com>
2023-05-23 18:20:23 +0900

    fix: ローディング変える

--- components/LoadingSign.vue
+++ components/LoadingSign.vue
@@ -4,60 +4,44 @@
 
 <style lang="scss" scoped>
   .loader,
-  .loader:before,
   .loader:after {
     border-radius: 50%;
-    width: 2.5em;
-    height: 2.5em;
-    -webkit-animation-fill-mode: both;
-    animation-fill-mode: both;
-    -webkit-animation: load7 1.8s infinite ease-in-out;
-    animation: load7 1.8s infinite ease-in-out;
+    width: 10em;
+    height: 10em;
   }
   .loader {
-    color: lightgray;
+    margin: 60px auto;
     font-size: 10px;
-    margin: 80px auto;
     position: relative;
     text-indent: -9999em;
+    border-top: 1.1em solid rgba(128, 128, 128, 0.2);
+    border-right: 1.1em solid rgba(128, 128, 128, 0.2);
+    border-bottom: 1.1em solid rgba(128, 128, 128, 0.2);
+    border-left: 1.1em solid #808080;
     -webkit-transform: translateZ(0);
     -ms-transform: translateZ(0);
     transform: translateZ(0);
-    -webkit-animation-delay: -0.16s;
-    animation-delay: -0.16s;
+    -webkit-animation: load8 1.1s infinite linear;
+    animation: load8 1.1s infinite linear;
   }
-  .loader:before,
-  .loader:after {
-    content: '';
-    position: absolute;
-    top: 0;
-  }
-  .loader:before {
-    left: -3.5em;
-    -webkit-animation-delay: -0.32s;
-    animation-delay: -0.32s;
-  }
-  .loader:after {
-    left: 3.5em;
-  }
-  @-webkit-keyframes load7 {
-    0%,
-    80%,
+  @-webkit-keyframes load8 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
     100% {
-      box-shadow: 0 2.5em 0 -1.3em;
-    }
-    40% {
-      box-shadow: 0 2.5em 0 0;
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
     }
   }
-  @keyframes load7 {
-    0%,
-    80%,
-    100% {
-      box-shadow: 0 2.5em 0 -1.3em;
+  @keyframes load8 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
     }
-    40% {
-      box-shadow: 0 2.5em 0 0;
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
     }
   }
 </style>
